20456c49cffbf7945c5266d33f8cd3f18c7524a4
Alexandre Heine <xandeaph@gmail.com>
2020-12-01 22:08:53 -0300

    adição de solicita ao model e alguns fix

--- CarRental/rental/models.py
+++ CarRental/rental/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Cliente(models.Model):
     usuario = models.OneToOneField('User', primary_key=True, on_delete=models.CASCADE)
-    telefone = models.CharField(max_length=15, required=False)
+    telefone = models.CharField(max_length=11, required=False, null=True)
     cpf = models.CharField(max_length=11)
     credito = models.FloatField(default=0)
 
@@ -12,7 +12,7 @@ class Funcionario(models.Model):
     usuario = models.OneToOneField('User', primary_key=True, on_delete=models.CASCADE)
     salario = models.FloatField()
     cpf = models.CharField(max_length=11)
-    telefone = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11, required=False, null=True)
     # horas contratadas por semana
     hh_semana = models.IntegerField()
 
@@ -34,9 +34,20 @@ class Aluguel(models.Model):
     data_ini = models.DateTimeField()
     data_fim = models.DateTimeField()
     # data de devolução considera que pode ser devolvido adiantado ou atrasado
-    data_devolucao = models.DateTimeField(required=False)
+    data_devolucao = models.DateTimeField(required=False, null=True)
     preco_aluguel = models.FloatField()
     taxa_atraso = models.FloatField(required=False)
     CONDICOES = ((0, 'Péssima'), (1, 'Ruim'), (2, 'OK'), (3, 'Boa'), (4, 'Semi-novo'), (5, 'Novo'))
     condicao_antes = models.IntegerField(choices=CONDICOES)
-    condicao_depois = models.IntegerField(choices=CONDICOES, required=False)
+    condicao_depois = models.IntegerField(choices=CONDICOES, required=False, null=True)
+
+
+class Solicita(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    valor = models.FloatField()
+    data_solicitacao = models.DateTimeField()
+    # funcionário que avalia a solicitação
+    funcionario = models.ForeignKey(Funcionario, required=False, null=True, on_delete=models.CASCADE)
+    # só é mudado após avaliação do funcionário
+    status = models.BooleanField(required=False, null=True)
+    data_avaliacao = models.DateTimeField(required=False, null=True)
